6dd463505cc6c36fd53aa5844ef6add3cbce01cd
Brendon Carrasquillo <brendon.wanderlust@gmail.com>
2023-10-29 18:17:43 -0400

    removed environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCI6QMsHj2odfPSkvAdx5Aw63Vp5_McL-M',
+    authDomain: 'travel-buddy-ionic-app.firebaseapp.com',
+    projectId: 'travel-buddy-ionic-app',
+    storageBucket: 'travel-buddy-ionic-app.appspot.com',
+    messagingSenderId: '212457633291',
+    appId: '1:212457633291:web:5bdb77ade1a078f550390e',
+    measurementId: 'G-YQNLFFQDFY',
+  },
 };
