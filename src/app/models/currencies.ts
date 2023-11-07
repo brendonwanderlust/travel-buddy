ec3bd506d76ff90fddc7bf7e7f7e2a6e6ed0b770
Brendon Carrasquillo <brendon.wanderlust@gmail.com>
2023-11-07 06:13:09 -0500

    added currencies and tabs changes

---
+++ src/app/models/currencies.ts
@@ -0,0 +1,326 @@
+export const currencyCodes = [
+  'AED', // United Arab Emirates Dirham
+  'AFN', // Afghanistan Afghani
+  'ALL', // Albania Lek
+  'AMD', // Armenia Dram
+  'ANG', // Netherlands Antilles Guilder
+  'AOA', // Angola Kwanza
+  'ARS', // Argentina Peso
+  'AUD', // Australia Dollar
+  'AWG', // Aruba Guilder
+  'AZN', // Azerbaijan Manat
+  'BAM', // Bosnia and Herzegovina Convertible Marka
+  'BBD', // Barbados Dollar
+  'BDT', // Bangladesh Taka
+  'BGN', // Bulgaria Lev
+  'BHD', // Bahrain Dinar
+  'BIF', // Burundi Franc
+  'BMD', // Bermuda Dollar
+  'BND', // Brunei Darussalam Dollar
+  'BOB', // Bolivia Bolíviano
+  'BRL', // Brazil Real
+  'BSD', // Bahamas Dollar
+  'BTN', // Bhutan Ngultrum
+  'BWP', // Botswana Pula
+  'BYN', // Belarus Ruble
+  'BZD', // Belize Dollar
+  'CAD', // Canada Dollar
+  'CDF', // Congo/Kinshasa Franc
+  'CHF', // Switzerland Franc
+  'CLP', // Chile Peso
+  'CNY', // China Yuan Renminbi
+  'COP', // Colombia Peso
+  'CRC', // Costa Rica Colon
+  'CUC', // Cuba Convertible Peso
+  'CUP', // Cuba Peso
+  'CVE', // Cape Verde Escudo
+  'CZK', // Czech Republic Koruna
+  'DJF', // Djibouti Franc
+  'DKK', // Denmark Krone
+  'DOP', // Dominican Republic Peso
+  'DZD', // Algeria Dinar
+  'EGP', // Egypt Pound
+  'ERN', // Eritrea Nakfa
+  'ETB', // Ethiopia Birr
+  'EUR', // Euro Member Countries
+  'FJD', // Fiji Dollar
+  'FKP', // Falkland Islands (Malvinas) Pound
+  'GBP', // United Kingdom Pound
+  'GEL', // Georgia Lari
+  'GGP', // Guernsey Pound
+  'GHS', // Ghana Cedi
+  'GIP', // Gibraltar Pound
+  'GMD', // Gambia Dalasi
+  'GNF', // Guinea Franc
+  'GTQ', // Guatemala Quetzal
+  'GYD', // Guyana Dollar
+  'HKD', // Hong Kong Dollar
+  'HNL', // Honduras Lempira
+  'HRK', // Croatia Kuna
+  'HTG', // Haiti Gourde
+  'HUF', // Hungary Forint
+  'IDR', // Indonesia Rupiah
+  'ILS', // Israel Shekel
+  'IMP', // Isle of Man Pound
+  'INR', // India Rupee
+  'IQD', // Iraq Dinar
+  'IRR', // Iran Rial
+  'ISK', // Iceland Krona
+  'JEP', // Jersey Pound
+  'JMD', // Jamaica Dollar
+  'JOD', // Jordan Dinar
+  'JPY', // Japan Yen
+  'KES', // Kenya Shilling
+  'KGS', // Kyrgyzstan Som
+  'KHR', // Cambodia Riel
+  'KMF', // Comorian Franc
+  'KPW', // Korea (North) Won
+  'KRW', // Korea (South) Won
+  'KWD', // Kuwait Dinar
+  'KYD', // Cayman Islands Dollar
+  'KZT', // Kazakhstan Tenge
+  'LAK', // Laos Kip
+  'LBP', // Lebanon Pound
+  'LKR', // Sri Lanka Rupee
+  'LRD', // Liberia Dollar
+  'LSL', // Lesotho Loti
+  'LYD', // Libya Dinar
+  'MAD', // Morocco Dirham
+  'MDL', // Moldova Leu
+  'MGA', // Madagascar Ariary
+  'MKD', // North Macedonia Denar
+  'MMK', // Myanmar (Burma) Kyat
+  'MNT', // Mongolia Tughrik
+  'MOP', // Macau Pataca
+  'MRU', // Mauritania Ouguiya
+  'MUR', // Mauritius Rupee
+  'MVR', // Maldives (Maldive Islands) Rufiyaa
+  'MWK', // Malawi Kwacha
+  'MXN', // Mexico Peso
+  'MYR', // Malaysia Ringgit
+  'MZN', // Mozambique Metical
+  'NAD', // Namibia Dollar
+  'NGN', // Nigeria Naira
+  'NIO', // Nicaragua Cordoba
+  'NOK', // Norway Krone
+  'NPR', // Nepal Rupee
+  'NZD', // New Zealand Dollar
+  'OMR', // Oman Rial
+  'PAB', // Panama Balboa
+  'PEN', // Peru Sol
+  'PGK', // Papua New Guinea Kina
+  'PHP', // Philippines Peso
+  'PKR', // Pakistan Rupee
+  'PLN', // Poland Zloty
+  'PYG', // Paraguay Guarani
+  'QAR', // Qatar Riyal
+  'RON', // Romania Leu
+  'RSD', // Serbia Dinar
+  'RUB', // Russia Ruble
+  'RWF', // Rwanda Franc
+  'SAR', // Saudi Arabia Riyal
+  'SBD', // Solomon Islands Dollar
+  'SCR', // Seychelles Rupee
+  'SDG', // Sudan Pound
+  'SEK', // Sweden Krona
+  'SGD', // Singapore Dollar
+  'SHP', // Saint Helena Pound
+  'SLL', // Sierra Leone Leone
+  'SOS', // Somalia Shilling
+  'SPL', // Seborga Luigino
+  'SRD', // Suriname Dollar
+  'STN', // São Tomé and Príncipe Dobra
+  'SVC', // El Salvador Colon
+  'SYP', // Syria Pound
+  'SZL', // Swaziland Lilangeni
+  'THB', // Thailand Baht
+  'TJS', // Tajikistan Somoni
+  'TMT', // Turkmenistan Manat
+  'TND', // Tunisia Dinar
+  'TOP', // Tonga Pa'anga
+  'TRY', // Turkey Lira
+  'TTD', // Trinidad and Tobago Dollar
+  'TVD', // Tuvalu Dollar
+  'TWD', // Taiwan New Dollar
+  'TZS', // Tanzania Shilling
+  'UAH', // Ukraine Hryvnia
+  'UGX', // Uganda Shilling
+  'USD', // United States Dollar
+  'UYU', // Uruguay Peso
+  'UZS', // Uzbekistan Som
+  'VEF', // Venezuela Bolívar
+  'VND', // Viet Nam Dong
+  'VUV', // Vanuatu Vatu
+  'WST', // Samoa Tala
+  'XAF', // Communauté Financière Africaine (BEAC) CFA Franc BEAC
+  'XCD', // East Caribbean Dollar
+  'XDR', // International Monetary Fund (IMF) Special Drawing Rights
+  'XOF', // Communauté Financière Africaine (BCEAO) Franc
+  'XPF', // Comptoirs Français du Pacifique (CFP) Franc
+  'YER', // Yemen Rial
+  'ZAR', // South Africa Rand
+  'ZMW', // Zambia Kwacha
+  'ZWD', // Zimbabwe Dollar
+];
+
+export const currencies = [
+  { currencyCode: 'AED', countryCode: 'AE' }, // United Arab Emirates Dirham
+  { currencyCode: 'AFN', countryCode: 'AF' }, // Afghanistan Afghani
+  { currencyCode: 'ALL', countryCode: 'AL' }, // Albania Lek
+  { currencyCode: 'AMD', countryCode: 'AM' }, // Armenia Dram
+  { currencyCode: 'ANG', countryCode: 'AN' }, // Netherlands Antilles Guilder
+  { currencyCode: 'AOA', countryCode: 'AO' }, // Angola Kwanza
+  { currencyCode: 'ARS', countryCode: 'AR' }, // Argentina Peso
+  { currencyCode: 'AUD', countryCode: 'AU' }, // Australia Dollar
+  { currencyCode: 'AWG', countryCode: 'AW' }, // Aruba Guilder
+  { currencyCode: 'AZN', countryCode: 'AZ' }, // Azerbaijan Manat
+  { currencyCode: 'BAM', countryCode: 'BA' }, // Bosnia and Herzegovina Convertible Marka
+  { currencyCode: 'BBD', countryCode: 'BB' }, // Barbados Dollar
+  { currencyCode: 'BDT', countryCode: 'BD' }, // Bangladesh Taka
+  { currencyCode: 'BGN', countryCode: 'BG' }, // Bulgaria Lev
+  { currencyCode: 'BHD', countryCode: 'BH' }, // Bahrain Dinar
+  { currencyCode: 'BIF', countryCode: 'BI' }, // Burundi Franc
+  { currencyCode: 'BMD', countryCode: 'BM' }, // Bermuda Dollar
+  { currencyCode: 'BND', countryCode: 'BN' }, // Brunei Darussalam Dollar
+  { currencyCode: 'BOB', countryCode: 'BO' }, // Bolivia Bolíviano
+  { currencyCode: 'BRL', countryCode: 'BR' }, // Brazil Real
+  { currencyCode: 'BSD', countryCode: 'BS' }, // Bahamas Dollar
+  { currencyCode: 'BTN', countryCode: 'BT' }, // Bhutan Ngultrum
+  { currencyCode: 'BWP', countryCode: 'BW' }, // Botswana Pula
+  { currencyCode: 'BYN', countryCode: 'BY' }, // Belarus Ruble
+  { currencyCode: 'BZD', countryCode: 'BZ' }, // Belize Dollar
+  { currencyCode: 'CAD', countryCode: 'CA' }, // Canada Dollar
+  { currencyCode: 'CDF', countryCode: 'CD' }, // Congo/Kinshasa Franc
+  { currencyCode: 'CHF', countryCode: 'CH' }, // Switzerland Franc
+  { currencyCode: 'CLP', countryCode: 'CL' }, // Chile Peso
+  { currencyCode: 'CNY', countryCode: 'CN' }, // China Yuan Renminbi
+  { currencyCode: 'COP', countryCode: 'CO' }, // Colombia Peso
+  { currencyCode: 'CRC', countryCode: 'CR' }, // Costa Rica Colon
+  { currencyCode: 'CUC', countryCode: 'CU' }, // Cuba Convertible Peso
+  { currencyCode: 'CUP', countryCode: 'CU' }, // Cuba Peso
+  { currencyCode: 'CVE', countryCode: 'CV' }, // Cape Verde Escudo
+  { currencyCode: 'CZK', countryCode: 'CZ' }, // Czech Republic Koruna
+  { currencyCode: 'DJF', countryCode: 'DJ' }, // Djibouti Franc
+  { currencyCode: 'DKK', countryCode: 'DK' }, // Denmark Krone
+  { currencyCode: 'DOP', countryCode: 'DO' }, // Dominican Republic Peso
+  { currencyCode: 'DZD', countryCode: 'DZ' }, // Algeria Dinar
+  { currencyCode: 'EGP', countryCode: 'EG' }, // Egypt Pound
+  { currencyCode: 'ERN', countryCode: 'ER' }, // Eritrea Nakfa
+  { currencyCode: 'ETB', countryCode: 'ET' }, // Ethiopia Birr
+  { currencyCode: 'EUR', countryCode: 'DE' }, // Euro Member Countries
+  { currencyCode: 'FJD', countryCode: 'FJ' }, // Fiji Dollar
+  { currencyCode: 'FKP', countryCode: 'FK' }, // Falkland Islands (Malvinas) Pound
+  { currencyCode: 'GBP', countryCode: 'GB' }, // United Kingdom Pound
+  { currencyCode: 'GEL', countryCode: 'GE' }, // Georgia Lari
+  { currencyCode: 'GGP', countryCode: 'GG' }, // Guernsey Pound
+  { currencyCode: 'GHS', countryCode: 'GH' }, // Ghana Cedi
+  { currencyCode: 'GIP', countryCode: 'GI' }, // Gibraltar Pound
+  { currencyCode: 'GMD', countryCode: 'GM' }, // Gambia Dalasi
+  { currencyCode: 'GNF', countryCode: 'GN' }, // Guinea Franc
+  { currencyCode: 'GTQ', countryCode: 'GT' }, // Guatemala Quetzal
+  { currencyCode: 'GYD', countryCode: 'GY' }, // Guyana Dollar
+  { currencyCode: 'HKD', countryCode: 'HK' }, // Hong Kong Dollar
+  { currencyCode: 'HNL', countryCode: 'HN' }, // Honduras Lempira
+  { currencyCode: 'HRK', countryCode: 'HR' }, // Croatia Kuna
+  { currencyCode: 'HTG', countryCode: 'HT' }, // Haiti Gourde
+  { currencyCode: 'HUF', countryCode: 'HU' }, // Hungary Forint
+  { currencyCode: 'IDR', countryCode: 'ID' }, // Indonesia Rupiah
+  { currencyCode: 'ILS', countryCode: 'IL' }, // Israel Shekel
+  { currencyCode: 'IMP', countryCode: 'IM' }, // Isle of Man Pound
+  { currencyCode: 'INR', countryCode: 'IN' }, // India Rupee
+  { currencyCode: 'IQD', countryCode: 'IQ' }, // Iraq Dinar
+  { currencyCode: 'IRR', countryCode: 'IR' }, // Iran Rial
+  { currencyCode: 'ISK', countryCode: 'IS' }, // Iceland Krona
+  { currencyCode: 'JEP', countryCode: 'JE' }, // Jersey Pound
+  { currencyCode: 'JMD', countryCode: 'JM' }, // Jamaica Dollar
+  { currencyCode: 'JOD', countryCode: 'JO' }, // Jordan Dinar
+  { currencyCode: 'JPY', countryCode: 'JP' }, // Japan Yen
+  { currencyCode: 'KES', countryCode: 'KE' }, // Kenya Shilling
+  { currencyCode: 'KGS', countryCode: 'KG' }, // Kyrgyzstan Som
+  { currencyCode: 'KHR', countryCode: 'KH' }, // Cambodia Riel
+  { currencyCode: 'KMF', countryCode: 'KM' }, // Comorian Franc
+  { currencyCode: 'KPW', countryCode: 'KP' }, // Korea (North) Won
+  { currencyCode: 'KRW', countryCode: 'KR' }, // Korea (South) Won
+  { currencyCode: 'KWD', countryCode: 'KW' }, // Kuwait Dinar
+  { currencyCode: 'KYD', countryCode: 'KY' }, // Cayman Islands Dollar
+  { currencyCode: 'KZT', countryCode: 'KZ' }, // Kazakhstan Tenge
+  { currencyCode: 'LAK', countryCode: 'LA' }, // Laos Kip
+  { currencyCode: 'LBP', countryCode: 'LB' }, // Lebanon Pound
+  { currencyCode: 'LKR', countryCode: 'LK' }, // Sri Lanka Rupee
+  { currencyCode: 'LRD', countryCode: 'LR' }, // Liberia Dollar
+  { currencyCode: 'LSL', countryCode: 'LS' }, // Lesotho Loti
+  { currencyCode: 'LYD', countryCode: 'LY' }, // Libya Dinar
+  { currencyCode: 'MAD', countryCode: 'MA' }, // Morocco Dirham
+  { currencyCode: 'MDL', countryCode: 'MD' }, // Moldova Leu
+  { currencyCode: 'MGA', countryCode: 'MG' }, // Madagascar Ariary
+  { currencyCode: 'MKD', countryCode: 'MK' }, // North Macedonia Denar
+  { currencyCode: 'MMK', countryCode: 'MM' }, // Myanmar (Burma) Kyat
+  { currencyCode: 'MNT', countryCode: 'MN' }, // Mongolia Tughrik
+  { currencyCode: 'MOP', countryCode: 'MO' }, // Macau Pataca
+  { currencyCode: 'MRU', countryCode: 'MR' }, // Mauritania Ouguiya
+  { currencyCode: 'MUR', countryCode: 'MU' }, // Mauritius Rupee
+  { currencyCode: 'MVR', countryCode: 'MV' }, // Maldives (Maldive Islands) Rufiyaa
+  { currencyCode: 'MWK', countryCode: 'MW' }, // Malawi Kwacha
+  { currencyCode: 'MXN', countryCode: 'MX' }, // Mexico Peso
+  { currencyCode: 'MYR', countryCode: 'MY' }, // Malaysia Ringgit
+  { currencyCode: 'MZN', countryCode: 'MZ' }, // Mozambique Metical
+  { currencyCode: 'NAD', countryCode: 'NA' }, // Namibia Dollar
+  { currencyCode: 'NGN', countryCode: 'NG' }, // Nigeria Naira
+  { currencyCode: 'NIO', countryCode: 'NI' }, // Nicaragua Cordoba
+  { currencyCode: 'NOK', countryCode: 'NO' }, // Norway Krone
+  { currencyCode: 'NPR', countryCode: 'NP' }, // Nepal Rupee
+  { currencyCode: 'NZD', countryCode: 'NZ' }, // New Zealand Dollar
+  { currencyCode: 'OMR', countryCode: 'OM' }, // Oman Rial
+  { currencyCode: 'PAB', countryCode: 'PA' }, // Panama Balboa
+  { currencyCode: 'PEN', countryCode: 'PE' }, // Peru Sol
+  { currencyCode: 'PGK', countryCode: 'PG' }, // Papua New Guinea Kina
+  { currencyCode: 'PHP', countryCode: 'PH' }, // Philippines Peso
+  { currencyCode: 'PKR', countryCode: 'PK' }, // Pakistan Rupee
+  { currencyCode: 'PLN', countryCode: 'PL' }, // Poland Zloty
+  { currencyCode: 'PYG', countryCode: 'PY' }, // Paraguay Guarani
+  { currencyCode: 'QAR', countryCode: 'QA' }, // Qatar Riyal
+  { currencyCode: 'RON', countryCode: 'RO' }, // Romania Leu
+  { currencyCode: 'RSD', countryCode: 'RS' }, // Serbia Dinar
+  { currencyCode: 'RUB', countryCode: 'RU' }, // Russia Ruble
+  { currencyCode: 'RWF', countryCode: 'RW' }, // Rwanda Franc
+  { currencyCode: 'SAR', countryCode: 'SA' }, // Saudi Arabia Riyal
+  { currencyCode: 'SBD', countryCode: 'SB' }, // Solomon Islands Dollar
+  { currencyCode: 'SCR', countryCode: 'SC' }, // Seychelles Rupee
+  { currencyCode: 'SDG', countryCode: 'SD' }, // Sudan Pound
+  { currencyCode: 'SEK', countryCode: 'SE' }, // Sweden Krona
+  { currencyCode: 'SGD', countryCode: 'SG' }, // Singapore Dollar
+  { currencyCode: 'SHP', countryCode: 'SH' }, // Saint Helena Pound
+  { currencyCode: 'SLL', countryCode: 'SL' }, // Sierra Leone Leone
+  { currencyCode: 'SOS', countryCode: 'SO' }, // Somalia Shilling
+  { currencyCode: 'SRD', countryCode: 'SR' }, // Suriname Dollar
+  { currencyCode: 'STN', countryCode: 'ST' }, // São Tomé and Príncipe Dobra
+  { currencyCode: 'SVC', countryCode: 'SV' }, // El Salvador Colon
+  { currencyCode: 'SYP', countryCode: 'SY' }, // Syria Pound
+  { currencyCode: 'SZL', countryCode: 'SZ' }, // Swaziland Lilangeni
+  { currencyCode: 'THB', countryCode: 'TH' }, // Thailand Baht
+  { currencyCode: 'TJS', countryCode: 'TJ' }, // Tajikistan Somoni
+  { currencyCode: 'TMT', countryCode: 'TM' }, // Turkmenistan Manat
+  { currencyCode: 'TND', countryCode: 'TN' }, // Tunisia Dinar
+  { currencyCode: 'TOP', countryCode: 'TO' }, // Tonga Pa'anga
+  { currencyCode: 'TRY', countryCode: 'TR' }, // Turkey Lira
+  { currencyCode: 'TTD', countryCode: 'TT' }, // Trinidad and Tobago Dollar
+  { currencyCode: 'TWD', countryCode: 'TW' }, // Taiwan New Dollar
+  { currencyCode: 'TZS', countryCode: 'TZ' }, // Tanzania Shilling
+  { currencyCode: 'UAH', countryCode: 'UA' }, // Ukraine Hryvnia
+  { currencyCode: 'UGX', countryCode: 'UG' }, // Uganda Shilling
+  { currencyCode: 'USD', countryCode: 'US' }, // United States Dollar
+  { currencyCode: 'UYU', countryCode: 'UY' }, // Uruguay Peso
+  { currencyCode: 'UZS', countryCode: 'UZ' }, // Uzbekistan Som
+  { currencyCode: 'VEF', countryCode: 'VE' }, // Venezuela Bolívar
+  { currencyCode: 'VND', countryCode: 'VN' }, // Viet Nam Dong
+  { currencyCode: 'VUV', countryCode: 'VU' }, // Vanuatu Vatu
+  { currencyCode: 'WST', countryCode: 'WS' }, // Samoa Tala
+  { currencyCode: 'XAF', countryCode: 'CM' }, // Communauté Financière Africaine (BEAC) CFA Franc BEAC
+  { currencyCode: 'XCD', countryCode: 'AG' }, // East Caribbean Dollar
+  { currencyCode: 'XOF', countryCode: 'CI' }, // Communauté Financière Africaine (BCEAO) Franc
+  { currencyCode: 'XPF', countryCode: 'PF' }, // Comptoirs Français du Pacifique (CFP) Franc
+  { currencyCode: 'YER', countryCode: 'YE' }, // Yemen Rial
+  { currencyCode: 'ZAR', countryCode: 'ZA' }, // South Africa Rand
+  { currencyCode: 'ZMW', countryCode: 'ZM' }, // Zambia Kwacha
+  { currencyCode: 'ZWD', countryCode: 'ZW' }, // Zimbabwe Dollar
+];
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,38 +1,8 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss'],
 })
-export class TabsPage implements OnInit, OnDestroy {
-  constructor(private activatedRoute: ActivatedRoute, private router: Router) {}
-
-  ngOnInit() {
-    this.setTitle(this.router.routerState.snapshot.url);
-    this.navSubscription = this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        this.setTitle(event.url);
-      }
-    });
-  }
-
-  title!: string;
-  navSubscription: Subscription | undefined;
-
-  setTitle(tab: string) {
-    switch (tab) {
-      case '/tabs/currency-convert':
-        this.title = 'Currency';
-        break;
-      default:
-        this.title = '';
-    }
-  }
-
-  ngOnDestroy() {
-    this.navSubscription!.unsubscribe();
-  }
-}
+export class TabsPage {}
